a5bfde8db59642b45c97b7c98bc01c520260ac49
BramDC <bram.decooman@gmail.com>
2017-10-23 20:14:11 +0200

    Task 1 implemented

--- task1/src/main/scala/Account.scala
+++ task1/src/main/scala/Account.scala
@@ -1,5 +1,15 @@
+import exceptions._
+
 class Account(initialBalance: Double, val uid: Int = Bank getUniqueId) {
-  def withdraw(amount: Double): Unit = ??? // Implement
-  def deposit(amount: Double): Unit = ??? // Implement
-  def getBalanceAmount: Double = ??? // Implement
+  private var balance: Double = initialBalance
+  def withdraw(amount: Double): Unit = this.synchronized {
+    if (amount < 0) throw new IllegalAmountException("You cannot withdraw a negative amount of money!")
+    if (amount > balance) throw new NoSufficientFundsException("Your account has insufficient funds to withdraw this amount of money.")
+    balance -= amount
+  }
+  def deposit(amount: Double): Unit = this.synchronized {
+    if (amount < 0) throw new IllegalAmountException("You cannot deposit a negative amount of money!")
+    balance += amount
+  }
+  def getBalanceAmount: Double = balance
 }
--- task1/src/main/scala/Bank.scala
+++ task1/src/main/scala/Bank.scala
@@ -2,10 +2,21 @@ object Bank {
 
   private var idCounter: Int = 0
 
-  def transaction(from: Account, to: Account, amount: Double): Unit = ??? // Implement
+  def transaction(from: Account, to: Account, amount: Double): Unit = {
+    if(from.uid<to.uid)// Prevent deadlock
+      from.synchronized{to.synchronized{
+        from.withdraw(amount)// Withdraw will throw exception if invalid amount/insufficient funds
+        to.deposit(amount)// Deposit cannot fail anymore (since withdraw would have thrown invalid amount exception otherwise)
+      }}
+    else
+      to.synchronized{from.synchronized{
+        from.withdraw(amount)
+        to.deposit(amount)
+      }}
+  }
 
-  def getUniqueId: Int = {
-    idCounter += 1 // Can this be improved?
+  def getUniqueId: Int = this.synchronized{
+    idCounter += 1 // Can this be improved? -> yes, use synchronization to prevent duplicate 'unique' ids in multithreaded environment
     idCounter
   }
 
